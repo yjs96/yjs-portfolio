a37716625883e5ca9fe28d9e3fda754869c1b139
YJS96 <wnt9864@hotmail.com>
2024-05-03 01:15:03 +0900

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
+export default function Home() {
+  const pageNumber = 20;
+  return (
+    <>
+      <div className="w-screen h-screen hidden justify-center items-center overflow-hidden lg:flex">
+        <Carousel className="w-[calc(100%-200px)]">
+          <CarouselContent>
+            {Array.from({ length: pageNumber }, (_, index) => (
+              <CarouselItem>
+                <Image
+                  src={`/${index}.png`}
+                  width="2880"
+                  height="1620"
+                  alt={index.toString()}
+                ></Image>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+      <div className="w-screen h-screen flex justify-center items-center overflow-hidden lg:hidden">
+        <Carousel className='w-full'>
+          <CarouselContent>
+            {Array.from({ length: pageNumber }, (_, index) => (
+              <CarouselItem>
+                <Image
+                  src={`/${index}.png`}
+                  width="1920"
+                  height="1080"
+                  alt={index.toString()}
+                ></Image>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </>
+  );
+}
